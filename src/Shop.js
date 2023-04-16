6fed3632b23c8298fc41013ce65e982bbb11538c
Shahid Khan <shahidsaifi88@gmail.com>
2023-04-16 12:56:03 +0530

    video74 completed

---
+++ src/Shop.js
@@ -0,0 +1,13 @@
+
+const Shop = () => {
+    return (
+        <div>
+            <h2>Buy Adidas Shoes 50/-</h2>
+            <button className="btn btn-primary mx-2">-</button>
+            Add to cart
+            <button className="btn btn-primary mx-2">+</button>
+        </div>
+    )
+}
+
+export default Shop
